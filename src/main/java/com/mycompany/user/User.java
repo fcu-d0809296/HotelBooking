c413b0c6d3734703660639ede7751ddb2a09d622
jw <jiannweyfcutw@mail.fcu.edu.tw>
2021-12-29 01:41:44 +0800

    first commit

---
+++ src/main/java/com/mycompany/user/User.java
@@ -0,0 +1,34 @@
+package com.mycompany.user;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@SuppressWarnings("ALL")
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 45, nullable = false, name = "name")
+    private String userName;
+
+    @Column(length = 45, nullable = false, name = "email")
+    private String userEmail;
+
+    @Column(length = 16, nullable = false, name = "password")
+    private String userPassword;
+
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "id=" + getId() +
+//
+//                ", firstName='" + getFirstName() + '\'' +
+//                '}';
+//    }
+
+}
